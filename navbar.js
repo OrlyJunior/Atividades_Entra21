dedd7eeb39bce3a63ae860d30d674040e4d02d41
unknown <orlyjunior21@gmail.com>
2023-09-15 21:49:51 -0300

    Sexto commit da atividade do site com HTML, CSS e JS, agora com todas as páginas funcionais, faltando apensas adicionar o CSS e melhorias em algumas páginas

---
+++ navbar.js
@@ -0,0 +1,10 @@
+document.write("<div class='primeiro'>");
+document.write("<a>Navegação</a>");
+document.write("<nav id='hidden'>");
+document.write("<a id='um' href='atividade1.html'>Início</a>");
+document.write("<a id='um' href='idade.html'>Idade</a>");
+document.write("<a id='um' href='notas.html'>Notas</a>");
+document.write("<a id='um' href='imc.html'>IMC</a>");
+document.write("<a id='um' href='salario.html'>Salario</a>")
+document.write("</nav>");
+document.write("</div>");
