e7823f4469484e647b952907f578273b391bdd2d
unknown <orlyjunior21@gmail.com>
2023-09-16 23:42:47 -0300

    Oitavo commit a atividade do site com HTML, CSS e JS, agora com melhorias em todas as páginas, faltando apenas a verificação de campos vazios nas páginas salario e IMC

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 document.write("<div class='primeiro'>");
 document.write("<a>Navegação</a>");
 document.write("<nav id='hidden'>");
-document.write("<a id='um' href='atividade1.html'>Início</a>");
+document.write("<a id='um' href='menu.html'>Início</a>");
 document.write("<a id='um' href='idade.html'>Idade</a>");
 document.write("<a id='um' href='notas.html'>Notas</a>");
 document.write("<a id='um' href='imc.html'>IMC</a>");
