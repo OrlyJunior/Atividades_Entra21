6ba5a62bc6de7f463284bb0ed0503865747bf528
Orly Antunes Borges Junior <orly.junior@proway.treina>
2023-09-25 16:39:15 -0300

    Segunda versão do site dos números, agora podendo escolher mostrar os números em ordem crescente, decrescente ou na ordem digitada, e com a função de achar um número dentro do array.

--- arrayNumeros.js
+++ arrayNumeros.js
@@ -1,5 +1,5 @@
-
 var lista = [];
+var listaO = [];
 var html = ``;
 
 function add() {
@@ -8,22 +8,63 @@ function add() {
     num = parseInt(num);
 
     lista.unshift(num);
+    listaO.unshift(num);
+}
+
+function acharNumero() {
+    var achar = document.getElementById("achar").value;
 
     for (var i = 0; i < lista.length; i++) {
-        for (var x = 0; x < lista.length - i; x++) {
-            if (lista[x] > lista[x + 1]) {
-                var mudar = lista[x];
-                lista[x] = lista[x + 1];
-                lista[x + 1] = mudar;
-            }
+        if (lista[i] == achar) {
+            document.getElementById("achado").innerHTML = `<p>O número ${achar} está na posição ${i + 1} do array`;
         }
     }
 }
 
-function mostrar() {
-    for (var i = 0; i < lista.length; i++) {
-        html += `<li>${lista[i]}</li>`;
-        var list = document.getElementById("list");
-        list.innerHTML = html;
+function ordenar(m) {
+    document.getElementById("list").innerHTML = ``;
+
+    var maneira = m.innerHTML;
+    if (maneira == "Crescente") {
+        for (var i = 0; i < lista.length; i++) {
+            for (var x = 0; x < lista.length - i; x++) {
+                if (lista[x] > lista[x + 1]) {
+                    var mudar = lista[x];
+                    lista[x] = lista[x + 1];
+                    lista[x + 1] = mudar;
+                }
+            }
+        }
+
+        for (var i = 0; i < lista.length; i++) {
+            document.getElementById("list").innerHTML += `<li>${lista[i]}</li>`;
+        }
     }
+
+    if (maneira == "Decrescente") {
+        for (var i = 0; i < lista.length; i++) {
+            for (var x = 0; x < lista.length - i; x++) {
+                if (lista[x] < lista[x + 1]) {
+                    var mudar = lista[x];
+                    lista[x] = lista[x + 1];
+                    lista[x + 1] = mudar;
+                }
+            }
+        }
+
+        for (var i = 0; i < lista.length; i++) {
+            document.getElementById("list").innerHTML += `<li>${lista[i]}</li>`;
+        }
+    }
+
+    if (maneira == "Original") {
+        for (var i = 0; i < listaO.length; i++) {
+            document.getElementById("list").innerHTML += `<li>${listaO[i]}</li>`;
+        }
+    }
+}
+
+function remover() {
+    var position = document.getElementById("remover").value;
+
 }
