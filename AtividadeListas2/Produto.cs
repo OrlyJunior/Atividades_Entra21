17ac0e30925fa705eca7f03174b32d0076dca720
Orly Antunes Borges Junior <orly.junior@proway.treina>
2023-11-08 17:28:13 -0300

    Atividade 2 iniciada, mas ainda com bugs que devem ser corrigidos.

---
+++ AtividadeListas2/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeListas2
+{
+    class Produto
+    {
+        public int id { get; set; }
+        public string desc { get; set; }
+        public double preco { get; set; }
+
+
+
+        public string toString()
+        {
+            return $"Id: {id}\nDescrição: {desc}\nPreço: {preco}\n";
+        }
+    }
+}
---
+++ AtividadeListas2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AtividadeListas2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<Produto> produtos = new LinkedList<Produto>();
+
+            string p = "S";
+
+            while (p == "S")
+            {
+                Produto produto = new Produto();
+
+                produto.id = int.Parse(Console.ReadLine());
+                produto.desc = Console.ReadLine();
+                produto.preco = double.Parse(Console.ReadLine());
+
+                if(produtos.Count > 0)
+                {
+                    adiciona(produtos, produto);
+                }
+                else
+                {
+                    produtos.AddFirst(produto);                   
+                }
+
+                Console.WriteLine("Continuar? S - sim");
+                p = Console.ReadLine().ToUpper();
+
+                foreach (Produto n in produtos)
+                {
+                    Console.WriteLine(n.toString());
+                }
+            }
+        }
+
+        static void adiciona(LinkedList<Produto> produtos, Produto produto)
+        {
+            foreach (Produto x in produtos)
+            {
+                if (x.desc == produto.desc)
+                {
+                    Console.WriteLine("Produto duplicado!");
+                }
+                else
+                {
+                    produtos.AddFirst(produto);
+                }
+            }
+        }
+    }
+}
