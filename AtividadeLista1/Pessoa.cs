d542ba4e5444b82e75c19549c912c7efa5604b5a
Orly Antunes Borges Junior <orly.junior@proway.treina>
2023-11-08 15:38:30 -0300

    Primeira atividade de Sorted Lists concluída

---
+++ AtividadeLista1/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeLista1
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+
+        public Pessoa() { }
+
+        public Pessoa(string Nome, int Numero)
+        {
+            this.Nome = Nome;
+            this.Numero = Numero;
+        }
+
+        public string toString()
+        {
+            return $"{Nome}\n{Numero}";
+        }
+    }
+}
---
+++ AtividadeLista1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AtividadeLista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, Pessoa> pessoas = new SortedList<int, Pessoa>();
+
+            add(pessoas);
+            
+            IList<int> chave = pessoas.Keys;
+            foreach(KeyValuePair<int, Pessoa> k in pessoas)
+            {
+                Console.WriteLine($"{k.Key}\n{k.Value.Nome}\n{k.Value.Numero}");
+            }
+        }
+
+        static void add(SortedList<int, Pessoa> lista)
+        {
+            int id = 1;
+
+            Pessoa person = new Pessoa("Cléber", 1234567);
+
+            lista.Add(id, person);
+
+            id++;
+        }
+    }
+}
