94e02124349910b21d69cecd0971b5703793d805
Orly Antunes Borges Junior <orly.junior@proway.treina>
2023-11-20 17:20:56 -0300

    Atividade de consulta ao banco de dados, agora com o método deletar funcional.

--- AcassaBancoDeDados/Dao/DaoContato.cs
+++ AcassaBancoDeDados/Dao/DaoContato.cs
@@ -1,16 +1,15 @@
 ﻿using AcessaBancoDeDados.Entidades;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AcessaBancoDeDados.Dao
 {
+
+
     public class DaoContato
     {
+        List<Contato> contatos = new();
+
         public bool salvar(Contato contato)
         {
             using (SqlConnection con = new())
@@ -57,17 +56,93 @@ namespace AcessaBancoDeDados.Dao
                 {
                     Contato ct = new();
 
+                    int idAnt = Convert.ToInt32(dr["ID"]);
+
                     ct.Id = Convert.ToInt32(dr["Id"]);
                     ct.Nome = Convert.ToString(dr["Nome"]);
                     ct.Numero = Convert.ToString(dr["Número"]);
                     ct.Cidade = Convert.ToString(dr["Cidade"]);
 
-                    Console.WriteLine(ct.toString());
+                    contatos.Add(ct);
+                }
+
+                foreach (Contato i in contatos)
+                {
+                    Console.WriteLine(i.toString());
                 }
 
                 Console.WriteLine();
 
                 return true;
+            }
+        }
+
+        public bool consultaId()
+        {
+            using (SqlConnection con = new())
+            {
+                Console.WriteLine("Qual id quer consultar?");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Contatos;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                SqlCommand cn = new();
+
+                con.Open();
+
+                cn.CommandType = CommandType.Text;
+
+                cn.CommandText = "select * from tb_contatos";
+
+                cn.Connection = con;
+
+                SqlDataReader dr;
+
+                dr = cn.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    if (Convert.ToInt32(dr["Id"]) == id)
+                    {
+                        Contato ct = new();
+
+                        ct.Id = Convert.ToInt32(dr["Id"]);
+                        ct.Nome = Convert.ToString(dr["Nome"]);
+                        ct.Numero = Convert.ToString(dr["Número"]);
+                        ct.Cidade = Convert.ToString(dr["Cidade"]);
+
+                        Console.WriteLine(ct.toString());
+
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+
+                return true;
+            }
+        }
+
+        public bool deletar()
+        {
+            using (SqlConnection con = new())
+            {
+                Console.WriteLine("Qual id deseja apagar?");
+                int id = int.Parse(Console.ReadLine());
+
+                con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Contatos;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                SqlCommand cn = new();
+
+                cn.CommandType = CommandType.Text;
+
+                cn.CommandText = $"delete from tb_contatos where Id = {id}";
+
+                con.Open();
+
+                cn.Connection = con;
+
+                return cn.ExecuteNonQuery() > 0;
             }
         }
     }
