37d9274251336340459b31db0a5d63b7958d64eb
Orly Antunes Borges Junior <orly.junior@proway.treina>
2023-11-28 17:05:48 -0300

    Primeiro teste de MVC, com páginas que listam uma lista de pessoa de maneiras diferentes.

---
+++ MVC2/Controllers/ConsultaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC2.Models;
+
+namespace MVC2.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        public IActionResult Consulta()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            pessoas.Add(new Pessoa() { Id = 1, Nome = "Cléber", Email = "cleber@gmail.com" });
+            pessoas.Add(new Pessoa() { Id = 2, Nome = "Ângela", Email = "angela@gmail.com" });
+            pessoas.Add(new Pessoa() { Id = 3, Nome = "Marcos", Email = "marcos@gmail.com" });
+
+            return View(pessoas[1]);
+        }
+    }
+}
---
+++ MVC2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC2.Models;
+using System.Diagnostics;
+
+namespace MVC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Teste()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            pessoas.Add(new Pessoa() { Id=1, Nome="Cléber", Email="cleber@gmail.com"});
+            pessoas.Add(new Pessoa() { Id = 2, Nome = "Ângela", Email = "angela@gmail.com" });
+            pessoas.Add(new Pessoa() { Id = 3, Nome = "Marcos", Email = "marcos@gmail.com" });
+
+            return View(pessoas);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
