460762fa1a36c92c3aa324ed4374b7b13f724d97
unknown <orlyjunior21@gmail.com>
2023-11-30 18:14:26 -0300

    Nova versão da atividade do CRUD com MVC, agora com a página inicial de compromissos iniciada

--- MVC3/Models/Compromisso.cs
+++ MVC3/Models/Compromisso.cs
@@ -4,7 +4,8 @@
     {
         //Id, data, hora, local, contato e status
         public int Id { get; set; }
-        public DateTime DataHora { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
         public int LocalId { get; set; }
         public int ContatoId { get; set; }
         public string Status { get; set; }
